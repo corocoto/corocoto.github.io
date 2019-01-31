7e81924929346f12316deafd5f317ec35a7aec5e
Artem <gusev2014russia@mail.ru>
2019-01-31 16:05:49 +0300

    Исправлены баги с preloader

--- company/js/main.js
+++ company/js/main.js
@@ -1,7 +1,10 @@
 window.onload = function() {
         const loader = document.getElementById('page-preloader');
         setInterval(() => {
-            if(loader.style.opacity==0)clearInterval();
+            if(loader.style.opacity==0){
+                clearInterval();
+                loader.classList.add('done');
+            }
             else loader.style.opacity -= 0.02;
         }, 30);
 };
