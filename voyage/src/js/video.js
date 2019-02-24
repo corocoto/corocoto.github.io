613f31662fac8d16c772a009fec03b162cb2b5bf
Artem <gusev2014russia@mail.ru>
2019-02-24 12:10:12 +0300

    Добавлена работа со слайдером

--- voyage/src/js/video.js
+++ voyage/src/js/video.js
@@ -1,16 +1,34 @@
 window.onload=function () {
   const countOfSlides = document.querySelectorAll('.slide').length,
         currentNumber = document.querySelector('.current_number'),
-        up_angle=document.querySelector('.fa-angle-up');
-  //console.log(countOfSlides);
-  //console.log(currentNumber);
-  for_i_enabled_false(up_angle);
-  currentNumber.innerText=`${currentNumber.innerText}/${countOfSlides}`;
+        up_angle=document.querySelector('.fa-angle-up'),
+        down_angle = document.querySelector('.fa-angle-down'),
+        slides= document.querySelectorAll('.slide');
+
+  let i=currentNumber.innerText;
+  currentNumber.innerText=`${i}/${countOfSlides}`;
+
+  down_angle.onclick=()=>{
+    switchSlides(slides[0],slides[1], up_angle, down_angle);
+    currentNumber.innerText=`${++i}/${countOfSlides}`;
+  };
+  up_angle.onclick=()=>{
+    switchSlides(slides[1],slides[0], down_angle, up_angle);
+    currentNumber.innerText=`${--i}/${countOfSlides}`;
+  };
 };
 
-function for_i_enabled_false(elem) {
-  elem.style.color='#ccc';
-  elem.style.cursor='default';
+
+
+function switchSlides(firstSlide, secondSlide, firstAngle, secondAngle) {
+  addRemoveClasses(firstSlide,secondSlide, 'active');
+  secondSlide.style.visibility='visible';
+  setTimeout(()=> firstSlide.style.visibility='hidden',4000);
+
+  addRemoveClasses(firstAngle, secondAngle, 'disable_angle')
 }
 
-
+function addRemoveClasses(firstElem, secondElem, nameOfClass) {
+  firstElem.classList.toggle(nameOfClass);
+  secondElem.classList.toggle(nameOfClass);
+}
