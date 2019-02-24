ee7fcea59560a9b74a4c8a9a4b7243fb21960d74
Artem <gusev2014russia@mail.ru>
2019-02-24 10:41:57 +0300

    Изменен внешний вид header'а

--- voyage/src/js/video.js
+++ voyage/src/js/video.js
@@ -1,6 +1,16 @@
 window.onload=function () {
-  const countOfSlides = document.getElementsByClassName('slide').length;
-  console.log(countOfSlides);
+  const countOfSlides = document.querySelectorAll('.slide').length,
+        currentNumber = document.querySelector('.current_number'),
+        up_angle=document.querySelector('.fa-angle-up');
+  //console.log(countOfSlides);
+  //console.log(currentNumber);
+  for_i_enabled_false(up_angle);
+  currentNumber.innerText=`${currentNumber.innerText}/${countOfSlides}`;
 };
+
+function for_i_enabled_false(elem) {
+  elem.style.color='#ccc';
+  elem.style.cursor='default';
+}
 
 
