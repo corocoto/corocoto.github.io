adb470163d71907e3842484f43e10d7f3738ff2e
Artem <gusev2014russia@mail.ru>
2019-04-18 18:09:39 +0300

    add media query

--- cars/src/js/index.js
+++ cars/src/js/index.js
@@ -2,6 +2,7 @@ import Animator from "./Animator.js";
 import Paginator from "./Paginator.js";
 import "./Navigator.js";
 import "../css/style.css";
+import "../css/media.css";
 
 let p=new Paginator();
 
