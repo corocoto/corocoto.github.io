2c1b2331b85b8e5cf89f6de01d52f4abd976cf6b
Artem <gusev2014russia@mail.ru>
2019-05-28 20:23:57 +0300

    add react application

---
+++ insta_react_app/precache-manifest.60d36c6a76189bbc1e08d806d60381fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c2f7bedcf337f977da9f85b32ddeac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82ea26aa91c002a3bf1a",
+    "url": "/static/css/main.272d7b72.chunk.css"
+  },
+  {
+    "revision": "eb35f3eb36adcbbb8f4c",
+    "url": "/static/js/2.4c82a623.chunk.js"
+  },
+  {
+    "revision": "82ea26aa91c002a3bf1a",
+    "url": "/static/js/main.4d0400f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "53bafee1173560fee9c2083021c87f30",
+    "url": "/static/media/loader.53bafee1.svg"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/static/media/logo.9e8a2b39.svg"
+  }
+]);
